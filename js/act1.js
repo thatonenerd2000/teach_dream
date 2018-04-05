ea82b2e754fd14535ffb5d927a47a211f9e574fb
thatonenerd2000 <chowdhuryshadman0@gmail.com>
2018-04-05 14:26:12 -0400

    done

--- js/act1.js
+++ js/act1.js
@@ -2,4 +2,5 @@ $(document).ready(function(){
     $(".scrolla").scrolla({
         mobile:true
     });
+    $('#sec3').paroller();
 })
--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
         lifeLike: true,
     });
     $('#sec1').paroller();
+    $('#sec3').paroller();
     $(".scrolla").scrolla({
         mobile:true
     });
