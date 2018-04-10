7f265fb9000f85e871edab4ace8c60c40b3d2ed9
Shadman Chowdhury <chowdhuryshadman0@gmail.com>
2018-04-10 13:39:24 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,5 @@
 /*global $*/
 /*global ready*/
 $(document).ready(function(){
-    $("#title").typeIt({
-        speed: 100,
-        lifeLike: true,
-    });
-    $('#sec1').paroller();
-    $('#sec3').paroller();
-    $(".scrolla").scrolla({
-        mobile:true
-    });
-    $(".dropdown-trigger").dropdown();
+
 })
