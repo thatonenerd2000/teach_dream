48237fb6770a1b7eb85568f0a27291ed0a7c34a9
Shadman <chowdhuryshadman0@gmail.com>
2019-01-16 22:36:08 -0500

    google translate

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,14 @@
 /*global ready*/
 $(document).ready(function() {
   $("#sec1").paroller();
-      $(".scrolla").scrolla({
-        mobile:true
-    });
+  $(".scrolla").scrolla({
+    mobile: true
+  });
 });
+
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement(
+    { pageLanguage: "en" },
+    "google_translate_element"
+  );
+}
